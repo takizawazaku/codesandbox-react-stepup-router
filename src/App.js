37de779362d840f986a6fc0957e857c981d9c317
takizawazaku <96064983+takizawazaku@users.noreply.github.com>
2022-02-05 23:37:58 +0900

    ネストされたページ遷移

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -18,10 +20,23 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/page1">
-          <Page1 />
-        </Route>
-        <Route exact path="/page2">
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
+        <Route path="/page2">
           <Page2 />
         </Route>
       </Switch>
